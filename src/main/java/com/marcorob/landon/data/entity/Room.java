f5cc46bb9eec227f8861b4efba05dcf5a2d757a0
MarcoRob <marcko.rp@gmail.com>
2017-12-18 22:10:17 -0600

    Initilize Project

---
+++ src/main/java/com/marcorob/landon/data/entity/Room.java
@@ -0,0 +1,19 @@
+package com.marcorob.landon.data.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="ROOM")
+public class Room {
+    @Id
+    @Column(name="ROOM_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name="NAME")
+    private String name;
+    @Column(name="ROOM_NUMBER")
+    private String number;
+    @Column(name="BED_INFO")
+    private String bedInfo;
+
+}
